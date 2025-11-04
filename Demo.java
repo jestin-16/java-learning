6d305cf29e3476483805a2e96ce4152183a55ce3
jestin16 <jestinshaji777@gmail.com>
2025-11-04 21:20:32 +0530

    intial push

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a=scanner.nextInt();
+        System.out.println("the value of a is "+ a);
+        System.out.println("Hello World!");
+    }
+}
