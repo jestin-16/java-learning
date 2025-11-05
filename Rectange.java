0798d064b77f83d78c52e76980586efabe42a189
jestin16 <jestinshaji777@gmail.com>
2025-11-05 22:21:47 +0530

    rectangle area

---
+++ Rectange.java
@@ -0,0 +1,33 @@
+public class Rectange {
+    private double length;
+    private double width;
+
+    public Rectange(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
