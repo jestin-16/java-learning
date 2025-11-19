1376b57e010e2fa6dbc8805a9aa021347128504f
jestin16 <jestinshaji777@gmail.com>
2025-11-19 22:05:47 +0530

    commit

---
+++ Pattern7.java
@@ -0,0 +1,12 @@
+public class Pattern7{
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
