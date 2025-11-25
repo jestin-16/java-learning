550775765de8aa9b97653061e7ecac9249462643
jestin16 <jestinshaji777@gmail.com>
2025-11-25 22:43:08 +0530

    if practise

---
+++ Mark.java
@@ -0,0 +1,11 @@
+public class Mark {
+    public static void main(String[] args) {
+        int mark=38;
+        if(mark>=35){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
