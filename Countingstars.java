e47b029f8526be46f80cda2abaa28ec34b72d7c4
jestin16 <jestinshaji777@gmail.com>
2026-01-14 21:07:56 +0530

    .

---
+++ Countingstars.java
@@ -0,0 +1,12 @@
+public class Countingstars {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows for the star pattern
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
