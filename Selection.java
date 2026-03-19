e4d998730eba484088596ffbfb1a5c2e501c73b5
jestin16 <jestinshaji777@gmail.com>
2026-03-19 16:18:30 +0530

    sort

---
+++ Selection.java
@@ -0,0 +1,12 @@
+public class Selection{
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
